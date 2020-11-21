081d4f96e529d10044ea7b33629af7f4b37b1cfe
Mert <mert.san3407@gmail.com>
2020-11-22 00:34:39 +0300

    added description and mapstyle

--- src/App.js
+++ src/App.js
@@ -2,26 +2,46 @@ import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
-import * as parksData from "./assets/skateboard.json"
-
+import * as parksData from "./assets/skateboard.json";
+import mapStyle from "./mapStyle";
 
 function Map(){
   const [selectedPark, setSelectedPark] = useState(null);
   return(
-    <GoogleMap defaultZoom={10} defaultCenter={{lat:45.421532, lng:-75.697189}} >
+    <GoogleMap defaultZoom={10} defaultCenter={{lat:45.421532, lng:-75.697189}} 
+      defaultOptions={{ styles: mapStyle}}
+    >
       
-      { parksData.features.map((park ) =>(
-        <Marker  key={park.properties.PARK_ID}
+      {parksData.features.map(park =>(
+      <Marker
+          key={park.properties.PARK_ID}
           position={{
-            lat:park.geometry.coordinates[1], 
+            lat: park.geometry.coordinates[1],
             lng:park.geometry.coordinates[0]
           }}
-          onClick={() => setSelectedPark(park)}
-
-        />
-      ) )}
+          onClick={()=>{
+            setSelectedPark(park);
+          }}
+          icon={{
+            url:"https://upload.wikimedia.org/wikipedia/commons/3/37/Skateboarding_pictogram.svg",
+            scaledSize: new window.google.maps.Size(25, 25)
+          }}
+      />
+      ))}
 
       {selectedPark && (
+        <InfoWindow
+          position={{
+            lat:selectedPark.geometry.coordinates[1], 
+            lng:selectedPark.geometry.coordinates[0]
+          }}     
+          onCloseClick={() => setSelectedPark(null)}   
+        >
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+      </InfoWindow>
         
       )}
 
