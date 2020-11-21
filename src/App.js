c8cefaa50b56674db8f51c83f78a685ac8eb03f5
Mert <mert.san3407@gmail.com>
2020-11-21 23:37:44 +0300

    added map and marker

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
+import * as parksData from "./assets/skateboard.json"
+
+
+function Map(){
+  const [selectedPark, setSelectedPark] = useState(null);
+  return(
+    <GoogleMap defaultZoom={10} defaultCenter={{lat:45.421532, lng:-75.697189}} >
+      
+      { parksData.features.map((park ) =>(
+        <Marker  key={park.properties.PARK_ID}
+          position={{
+            lat:park.geometry.coordinates[1], 
+            lng:park.geometry.coordinates[0]
+          }}
+          onClick={() => setSelectedPark(park)}
+
+        />
+      ) )}
+
+      {selectedPark && (
+        
+      )}
+
+
+    
+    </GoogleMap>
+
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height:"100vh" }}>
+
+      <WrappedMap 
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCMTH8Qd9kAWuTdZLKxziBracsztLa40tY"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+
+    </div>
+  );
+}
+
+export default App;
